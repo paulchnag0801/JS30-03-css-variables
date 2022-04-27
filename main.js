77ac5cea68f0180536c657cc19e91d29f6315c3e
paulchnag0801 <x1232178987@gmail.com>
2022-04-27 13:58:15 +0800

    finish project

---
+++ main.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll('.controls input')
+const spacingPx = document.querySelector(".spac")
+const inputSpacing = document.querySelector("#spacing")
+const blurPx = document.querySelector(".blu")
+const inputBlur = document.querySelector("#blur")
+
+function inputChange() {
+  const pxSize = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + pxSize)
+  spacingPx.innerHTML = `spacing:${inputSpacing.value}px`
+  blurPx.innerHTML = `blur:${inputBlur.value}px`
+}
+
+inputs.forEach(input => input.addEventListener("change", inputChange))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+
+
+
